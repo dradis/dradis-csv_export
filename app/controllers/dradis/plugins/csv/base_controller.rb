c7b4d7ece48360dd5343cc4f21ed51eee56ad3be
Daniel Martin <etd-github@nomejortu.com>
2017-02-28 10:27:39 +0100

    Make compativle with dradis-plugins 3.6

--- app/controllers/dradis/plugins/csv/base_controller.rb
+++ app/controllers/dradis/plugins/csv/base_controller.rb
@@ -5,13 +5,9 @@ module Dradis
 
         def index
           # these come from Export#create
-          export_manager_hash   = session[:export_manager].with_indifferent_access
-          content_service_class = export_manager_hash[:content_service].constantize
-
-          exporter = Dradis::Plugins::CSV::Exporter.new(
-            content_service: content_service_class.new(plugin: Dradis::Plugins::CSV)
-          )
-          csv = exporter.export(export_manager_hash)
+          export_manager_hash = session[:export_manager].with_indifferent_access
+          exporter            = Dradis::Plugins::CSV::Exporter.new
+          csv                 = exporter.export(export_manager_hash)
 
           send_data csv,
             disposition: 'inline',
