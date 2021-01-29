68d3067b174540f1ecc3ec9ae2fd44db2704b757
Daniel Martin <53006+etdsoft@users.noreply.github.com>
2021-01-29 11:19:51 +0100

    Improve syntax to please Rails 6's loader

--- lib/dradis/plugins/csv.rb
+++ lib/dradis/plugins/csv.rb
@@ -1,3 +1,10 @@
+module Dradis
+  module Plugins
+    module CVS
+    end
+  end
+end
+
 require 'dradis/plugins/csv/engine'
 require 'dradis/plugins/csv/exporter'
 require 'dradis/plugins/csv/version'
