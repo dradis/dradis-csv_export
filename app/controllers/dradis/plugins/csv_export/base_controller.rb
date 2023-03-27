850ab972d6140132f4d943725b4643f863a9cf4a
Aaron Manaloto <mail@aapomm.io>
2023-03-27 16:07:23 +0800

    Pass scope and validate

--- app/controllers/dradis/plugins/csv_export/base_controller.rb
+++ app/controllers/dradis/plugins/csv_export/base_controller.rb
@@ -2,18 +2,30 @@ module Dradis
   module Plugins
     module CSVExport
       class BaseController < Dradis::Plugins::Export::BaseController
+        before_action :validate_scope
 
         def index
-          exporter = Dradis::Plugins::CSVExport::Exporter.new(export_options)
-          csv      = exporter.export
+          exporter = Dradis::Plugins::CSVExport::Exporter.new(
+            export_options.merge(scope: @scope.to_sym)
+          )
+          csv = exporter.export
 
           send_data csv,
             disposition: 'inline',
             filename: "dradis_report-#{Time.now.to_i}.csv",
             type: 'text/csv'
         end
-      end
 
+        private
+
+        def validate_scope
+          @scope = params[:scope]
+
+          unless @scope == 'all' || @scope == 'published'
+            raise 'Something fishy is going on...'
+          end
+        end
+      end
     end
   end
 end
