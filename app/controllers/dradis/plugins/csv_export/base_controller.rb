9db1b713d25129dc75b3639987ecc4322bac3c4a
Aaron Manaloto <mail@aapomm.io>
2023-03-28 20:06:36 +0800

    Remove validation

--- app/controllers/dradis/plugins/csv_export/base_controller.rb
+++ app/controllers/dradis/plugins/csv_export/base_controller.rb
@@ -2,8 +2,6 @@ module Dradis
   module Plugins
     module CSVExport
       class BaseController < Dradis::Plugins::Export::BaseController
-        before_action :validate_scope
-
         def index
           exporter = Dradis::Plugins::CSVExport::Exporter.new(
             export_options.merge(scope: @scope.to_sym)
@@ -14,16 +12,6 @@ module Dradis
             disposition: 'inline',
             filename: "dradis_report-#{Time.now.to_i}.csv",
             type: 'text/csv'
-        end
-
-        private
-
-        def validate_scope
-          @scope = params[:scope]
-
-          unless @scope == 'all' || @scope == 'published'
-            raise 'Something fishy is going on...'
-          end
         end
       end
     end
