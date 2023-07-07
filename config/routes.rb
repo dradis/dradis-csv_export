61191635e059db6fd9a94137ac2c234213283b76
Caitlin <caitlinhenry.ss@gmail.com>
2023-07-07 09:38:33 -0400

    update report route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Dradis::Plugins::CSVExport::Engine.routes.draw do
   resources :projects, only: [] do
-    resource :report, only: [:create], path: '/export/csv'
+    resource :report, only: [:create], path: '/export/csv/reports'
   end
 end
